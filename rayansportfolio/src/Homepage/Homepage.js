1f27f468b66c8b579a5dc7668eacdb4e0e9e3a22
rae8108 <rayanbana01@gmail.com>
2025-01-23 20:36:57 +0000

    created and added content to homepage

---
+++ rayansportfolio/src/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { NavLink } from 'react-router-dom';
+import "./Homepage.css"
+
+export default function Homepage() {
+    return (
+        <div>
+         <h1 className=""><NavLink to="/">Rayan Bana</NavLink></h1>
+          <h3>United Kingdom</h3>
+          <div className="info">
+          Aspiring developer with a passion for building accessible web experiences.
+          </div>
+        </div>
+    )
+}
