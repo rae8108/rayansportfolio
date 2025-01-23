e5b85e01fc8fb1607179a78208a1d1d6d7d33fe2
rae8108 <rayanbana01@gmail.com>
2025-01-23 20:36:16 +0000

    created resume component

---
+++ rayansportfolio/src/Resume/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h2>Resume Section</h2>
+      <p>This is where your Resume will be listed.</p>
+    </div>
+  );
+}
