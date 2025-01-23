bdc7fc18b6ee1bd9502e3e2d5d0d1111ef6d540f
rae8108 <rayanbana01@gmail.com>
2025-01-23 20:36:37 +0000

    created project.js

---
+++ rayansportfolio/src/Projects/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <div>
+      <h2>Projects Section</h2>
+      <p>This is where your projects will be listed.</p>
+    </div>
+  );
+}
