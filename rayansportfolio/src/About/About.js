e3bc669a16c345d3798d7a610a0a41410c2b1d96
rae8108 <rayanbana01@gmail.com>
2025-01-24 18:57:08 +0000

    added an about me section

--- rayansportfolio/src/About/About.js
+++ rayansportfolio/src/About/About.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./About.css"
 
 export default function About() {
     return (
         <div>
-            about me section
-
+        <p>
+       I graduated with a Law (Hons) LLB in 2020 but soon realised that my true passion lay elsewhere. In search of a career that would allow me to be creative, I discovered software development. Over the past few years I have completed multiple bootcamps (SheCodes and Iungo’s Software Development), and I am now committed and eager to the art of building dynamic, responsive and user-friendly websites. 
+       </p>
+       <p>
+       Outside the bootcamps I am continuously learning and evolving in my development journey by focusing on expanding my skills through ongoing CPD opportunities. My most recent personal project involved building a bubble tea website with React, where I applied my growing skills and learned valuable lessons with each line of code. 
+       </p>
+       <p>
+       While my previous experience has primarily been in customer service and administration, I am now eager to use my newfound skills in web development to create innovative and creative websites. As for now I am actively seeking a role while pursuing additional developmental courses and opportunities to further enhance my skills, allowing me to  continue to create and contribute effectively in future roles. 
+        </p>
         </div>
     )
 }
