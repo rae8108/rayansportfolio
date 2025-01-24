89fa3e1be39f125325a71b123985de4fa7526567
rae8108 <rayanbana01@gmail.com>
2025-01-24 18:58:20 +0000

    created skills.js

---
+++ rayansportfolio/src/Resume/Skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Skills.css";
+
+export default function Skills() {
+  return (
+    <div className='skills-container'>
+    <h3>Skills</h3>
+    <div className='skills-grid'>
+        <p>Administration</p>
+        <p>Time Management</p>
+        <p>Adaptability</p>
+        <p>Interpersonal Skills</p>
+        <p>Problem Solving</p>
+        <p>Communication</p>
+        <p>Microsoft Office</p>
+        <p>Team Collaboration</p>
+        <p>Attention to Detail</p>
+    </div>
+    </div>
+  );
+}
