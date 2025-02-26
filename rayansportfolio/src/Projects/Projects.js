dc1ef6bdfc76ef89f03af59b973b337b124969f0
rae8108 <rayanbana01@gmail.com>
2025-02-26 14:39:16 +0000

    updated project.js

--- rayansportfolio/src/Projects/Projects.js
+++ rayansportfolio/src/Projects/Projects.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import "./Projects.css";
+import chaibubs from "../images/chaibubssc.jpeg";
 
 export default function Projects() {
   return (
     <div>
-      <h2>Projects Section</h2>
-      <p>This is where your projects will be listed.</p>
+      <h4>Projects</h4>
+      <div className='project-grid'>
+        <img src={chaibubs} alt="chaibubs site" className='chaibubs'/>
+        <div>
+        <h6>Project title</h6>
+        <p>Processed transactions. Worked efficiently and maintained a positive attitude within a fast-paced environment with strict attention to detail. Supervising a team of staff when a manager was unavailable.</p>
+        </div>
+      
+
+      </div>
     </div>
   );
 }
