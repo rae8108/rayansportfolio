fe2d0ca70678f167dbd5b366110bedb8d233ae42
rae8108 <rayanbana01@gmail.com>
2025-02-26 16:09:57 +0000

    added project images

--- rayansportfolio/src/Projects/Projects.js
+++ rayansportfolio/src/Projects/Projects.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import "./Projects.css";
-import chaibubs from "../images/chaibubssc.jpeg";
+import chaibubs from "../images/chaibubscard.jpeg";
+import dictionaryapp from "../images/dictionaryappcard.jpeg"
+import weatherapp from "../images/weatherappcard.jpeg"
+import stephensanchez from "../images/stephensanchezcard.jpeg"
+import positivequotes from "../images/positivequotescard.jpeg"
 
 export default function Projects() {
   return (
     <div>
       <h4>Projects</h4>
       <div className='project-grid'>
-        <img src={chaibubs} alt="chaibubs site" className='chaibubs'/>
+        <img src={chaibubs} alt="chaibubs site" className='projectimage'/>
         <div>
-        <h6>Project title</h6>
-        <p>Processed transactions. Worked efficiently and maintained a positive attitude within a fast-paced environment with strict attention to detail. Supervising a team of staff when a manager was unavailable.</p>
+        <h6 className='projecttitle'>Chaibubs</h6>
+        <p className='projectp'>Multi-page bubble tea website showcasing options to browse home, menu, products and site locations. Used carousel for images, grids and embedded google maps for each location. </p>
         </div>
-      
+      </div>
 
+      <div className='project-grid'>
+        <img src={dictionaryapp} alt="dictionaryapp" className='projectimage'/>
+        <div>
+        <h6 className='projecttitle'>Dictionary App </h6>
+        <p className='projectp'>Multi-page bubble tea website showcasing options to browse home, menu, products and site locations. Used carousel for images, grids and embedded google maps for each location. </p>
+        </div>
+      </div>
+
+      <div className='project-grid'>
+        <img src={weatherapp} alt="weatherapp" className='projectimage'/>
+        <div>
+        <h6 className='projecttitle'>Weather App </h6>
+        <p className='projectp'>Multi-page bubble tea website showcasing options to browse home, menu, products and site locations. Used carousel for images, grids and embedded google maps for each location. </p>
+        </div>
+      </div>
+
+      <div className='project-grid'>
+        <img src={stephensanchez} alt="stephensanchez" className='projectimage'/>
+        <div>
+        <h6 className='projecttitle'>Stephen Sanchez </h6>
+        <p className='projectp'>Multi-page bubble tea website showcasing options to browse home, menu, products and site locations. Used carousel for images, grids and embedded google maps for each location. </p>
+        </div>
+      </div>
+
+      <div className='project-grid'>
+        <img src={positivequotes} alt="positive quotes" className='projectimage'/>
+        <div>
+        <h6 className='projecttitle'>Positive Quotes Generator </h6>
+        <p className='projectp'>Multi-page bubble tea website showcasing options to browse home, menu, products and site locations. Used carousel for images, grids and embedded google maps for each location. </p>
+        </div>
       </div>
     </div>
   );
